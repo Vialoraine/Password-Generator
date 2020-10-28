90374db1341c7b89de27163b10050e0b5679bba9
Loreina Salalila <vialoraine.gg@gmail.com>
2020-10-28 10:19:02 -0700

    updated script.js

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~"];
 
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-space = [];
 
 var choices;
 
@@ -84,9 +82,6 @@ function generatePassword() {
   }
   else if (confirmLowercase) {
     choices = alpha;
-  }
-  else if (confirmUppercase) {
-    choices = space.concat(alpha2);
   };
 
   var password = [];
